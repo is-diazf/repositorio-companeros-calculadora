302d92613ad2892c0bc84db1ce1ce121c111f2e4
is-diazf <is.diazf@duocuc.cl>
2024-06-19 15:22:00 -0400

    Update repositorio.py

--- repositorio.py
+++ repositorio.py
@@ -1,8 +1,6 @@
 #este es el archivo py en el q trabajaremos con mis ñeros pa crear una calculadora entre todos
 #pq si la pone uno, la ponen todos
-<<<<<<< HEAD
+#THE GAME
 
 #pegate un saque compañero
-=======
-#THE GAME
->>>>>>> dc55c20c3e411c6a27a8d5292442227f98ef479a
+#creo que así se soluciona
